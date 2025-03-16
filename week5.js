493d179fd828e7fd279c78774813708029f6fd5c
Daniel Juniper <daniel-juniper@live.nmit.ac.nz>
2025-03-17 09:51:19 +1300

    working on somthing

---
+++ week5.js
@@ -0,0 +1,27 @@
+//NOTE - math object 
+/*
+in js the math object provieds lots of methods to work with numbers.
+*/
+
+const PI = Math.PI
+//console.log(PI)
+
+//ANCHOR -  rounding to the closest number 
+// if above .5 round it up if less .5 then round down 
+
+//console.log(Math.round(PI))
+//console.log(Math.round(9.81))
+
+//console.log(Math.floor(PI))
+//console.log(Math.ceil(PI))
+
+console.log(Math.min(-5,3,20,4,5,10)) // -5 , returns the mininum value 
+console.log(Math.max(-5,3,20,4,5,10)) // 20,returns the maximum value 
+
+const randNum = Math.random()
+console.log(randnom)
+
+// create a random number between 0 and -10 
+
+const num = Math.floor(Math.random() * 11)
+console.log(num)
